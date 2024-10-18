29248fd05b6e2560ca5f22ffc43e182417b4ec82
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2024-10-18 15:29:59 +0200

    Refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
         <div className="row">
           {data.map(person => {
             return (
-              <div key={person.id} className="col-md-4 mb-4">
+              <div key={person.id} className="col-md-16 mb-4">
                 <div className="card h-100">
                   <div className="card-body d-flex flex-column">
                     <h5 className="card-title">{person.name}</h5>
