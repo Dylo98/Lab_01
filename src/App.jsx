f561c8303e6e22f7af892668f02492d1f4521619
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2024-10-18 15:23:31 +0200

    Injection data from module-data to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,6 @@ import { data } from './module-data';
 function App() {
   const [count, setCount] = useState(0);
 
-  const randomName = data[Math.floor(Math.random() * data.length)];
-
   return (
     <>
       <div>
@@ -28,7 +26,24 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">{randomName}</p>
+      <div className="container mt-4">
+        <h1 className="text-center mb-4">Dane</h1>
+        <div className="row">
+          {data.map(person => {
+            return (
+              <div key={person.id} className="col-md-4 mb-4">
+                <div className="card h-100">
+                  <div className="card-body d-flex flex-column">
+                    <h5 className="card-title">{person.name}</h5>
+                    <p className="card-text">Birthday: {person.birth}</p>
+                    <p className="card-text">Eyes: {person.eyes}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
--- src/module-data.js
+++ src/module-data.js
@@ -1,1 +1,12 @@
-export const data = ["Dawid","Kamil","Kamil","Dawid","Marcin","Eryk","Wiola","Wiola","Dawid","Kinga",];
+export const data = [
+  {"id":1,"name":"Eryk","birth":"1981-08-26","eyes":"brown"},
+  {"id":2,"name":"Eryk","birth":"2014-08-08","eyes":"gray"},
+  {"id":3,"name":"Kinga","birth":"2008-01-06","eyes":"green"},
+  {"id":4,"name":"Wiola","birth":"1981-11-25","eyes":"blue"},
+  {"id":5,"name":"Dawid","birth":"1990-06-12","eyes":"blue"},
+  {"id":6,"name":"Marcin","birth":"1995-07-15","eyes":"gray"},
+  {"id":7,"name":"Adrian","birth":"1975-11-21","eyes":"gray"},
+  {"id":8,"name":"Dawid","birth":"2009-11-05","eyes":"blue"},
+  {"id":9,"name":"Wiola","birth":"2020-08-24","eyes":"hazel"},
+  {"id":10,"name":"Kamil","birth":"1976-04-28","eyes":"green"},
+];
