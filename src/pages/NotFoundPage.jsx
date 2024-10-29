98cfd08b1c62952c7542ea496fc0cb6ccf20e548
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2024-10-29 17:55:28 +0100

    Remove comments

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -10,16 +10,3 @@ const NotFoundPage = () => {
 };
 
 export default NotFoundPage;
-
-// import { Link } from 'react-router-dom';
-
-// const NotFoundPage = () => {
-//   return (
-//     <div style={{ textAlign: 'center', marginTop: '50px' }}>
-//       <h1>404 - Page Not Found</h1>
-//       <Link to="/">Go to Home</Link>
-//     </div>
-//   );
-// };
-
-// export default NotFoundPage;
