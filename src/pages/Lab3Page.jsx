9efdb2ba6122597de6ab119962cc1d7c99eb3447
Dylo98 <dawid.dyl@microsoft.wsei.edu.pl>
2024-12-13 18:00:16 +0100

    Add LocalStorage

--- src/pages/Lab3Page.jsx
+++ src/pages/Lab3Page.jsx
@@ -1,11 +1,15 @@
+import { useContext } from 'react';
 import FlexContainer from '../components/FlexContainer';
 import ProfileCard from '../components/ProfileCard';
+import AppContext from '../data/AppContext';
 
 function Lab3Page() {
+  const { items } = useContext(AppContext);
+
   return (
     <div>
       <h1>Laboratorium 3</h1>
-      <FlexContainer element={ProfileCard} />
+      <FlexContainer items={items} element={ProfileCard} />
     </div>
   );
 }
